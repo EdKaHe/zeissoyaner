accc011732594eb1ba9eefdf8c5b6f529a6e437a
buntstifte <shops@runbox.com>
2019-02-08 22:28:59 +0100

    version lib

---
+++ versions/deep_versions.py
@@ -0,0 +1,12 @@
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
+# pytorch
+import torch
+print('torch: %s' % pytorch.__version__)
