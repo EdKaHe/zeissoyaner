ea0cb7694e630fbdaffeb538318942210c2d1013
Wuest <ZOTWUE@zeiss.com>
2019-02-09 20:52:39 +0100

    added python venv and updated Flask app

--- application.py
+++ application.py
@@ -6,14 +6,31 @@ Created on Sat Feb  9 10:41:11 2019
 """
 
 
-from flask import Flask
+from flask import Flask, Response
+import os.path
+
 
 app = Flask(__name__)
 
+def get_file(filename):
+    try:
+        src = os.getcwd() + r"\\" + filename
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
+#Main page
 @app.route("/")
 def hello():
     return "Hello World!"
 
+#test subpage
+@app.route("/test")
+def testpage():
+    content = get_file("/Frontend/testfile.html")
+    return Response(content, mimetype="text/html")
+
 
 if __name__ == "__main__":
     app.run()
+
